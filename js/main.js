6ecdaaf3c2b9d166634ba16c7595da3d13956580
Zaur <zaurdysh@gmail.com>
2022-10-26 18:00:14 +0300

    commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+  $(".slider").slick({
+    prevArrow:
+      '<img class="slider__arrow slider__arrow_left" src="images/arrow-left.svg">',
+    nextArrow:
+      '<img class="slider__arrow slider__arrow_right" src="images/arrow-right.svg">',
+  });
+});
